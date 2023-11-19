98fcb732d5c0f20376579450c0c87dabe8338ef0
Gift Msigwa <gisacc54@Gifts-MacBook-Air.local>
2023-11-19 19:03:53 +0300

    db connection

---
+++ db/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$dbName="contactDb";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connected successful";
+
+}catch (PDOException $exception){
+    echo "Fail to connect, $exception";
+}
