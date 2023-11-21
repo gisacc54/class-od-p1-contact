b10856a8371c7ee54f91e1c8d711f6708eb154a5
Gift Msigwa <gisacc54@gos.local>
2023-11-21 19:06:08 +0300

    update

--- app/create.php
+++ app/create.php
@@ -8,6 +8,14 @@ if (isset($_POST['submit'])){
     $website = $_POST['website'];
     $address = $_POST['address'];
 
+    try {
+        $sql = "insert into contacts (name,phone_number,email,website,address) values ('$fullName','$phoneNumber','$email','$website','$address')";
+        $conn->exec($sql);
+        echo "Contact created successful";
+        header('location: ../');
+    }catch (PDOException $e){
+        echo "Error: ".$e->getMessage();
+    }
 }else{
     print "Not submitted";
 }
