646f78fcc307cf3cd730a634bd995c0841e46164
Gift Msigwa <gisacc54@gos.local>
2023-11-21 19:42:58 +0300

    update

---
+++ app/view-contact.php
@@ -0,0 +1,15 @@
+<?php
+
+
+require "../db/db.php";
+
+if ($_GET["id"]){
+    $contactId = $_GET['id'];
+    $sql = "Select * from contacts where id = $contactId";
+
+    $statement = $conn->prepare($sql);
+    $statement->execute();
+
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+}
